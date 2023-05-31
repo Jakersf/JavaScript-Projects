38b4e84d821ced4d53e49e1ca4a8b828a5ed9124
Jakersf <ecxzero1@gmail.com>
2023-05-31 06:12:43 -0500

    basic javascript code demonstration

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+var X = "Hello, World!";
+
+var A = "Barry's big \"day\" is tomorrow." + "-Gene"; 
+
+var B = "Concatenated" + "String" // This variable demonstrates a a concatenated string!
+
+var Sent1 ="This is the beginning of the string " //This declares the variable Sent1
+
+var Sent2="and this is the end of the string." //This declares the variable Sent2
+
+var Family = "Hills", Dad = "Hank", Mom = "Peggy", Son = "Bobby";
+
+document.write(Sent1 + Sent2) //This concatenates the variables Sent1 and Sent2, displaying them on the webpage.
+
+window.alert(A); //Creates a popup window displaying the string assigned to the variable A
+
+document.write(3 + 3);
